55d5c1764221d429b414d2ae361a5ea0389061f8
C-Christiaan <147825855+C-Christiaan@users.noreply.github.com>
2023-11-24 10:43:06 +0100

    Add files via upload
    Creating a diffrent version of the Bit Academy website

---
+++ Bootstrap-018bebda-018bebda/process.php
@@ -0,0 +1,180 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <title>Title</title>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=yes">
+
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+
+</head>
+
+<body class="bg-light text-success" font-family:georgia>
+  <header>
+    <div class="container-fluid">
+        <div class="row flex-nowrap">
+            <div class="col-auto col-md-2 col-xl-2 px-sm-2 px-0 bg-success">
+                <div class="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-dark min-vh-100">
+                    <a href="index.html"
+                        class="d-flex align-items-center pb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
+                        <span class="fs-5 d-none d-sm-inline"><img src="Bit.jpeg" alt="Logo hier" height="50px"></span>
+                    </a>
+                    <ul class="nav nav-tabs flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start"
+                        id="menu">
+                        <li>
+                            <a href="#opleiding" data-bs-toggle="collapse"
+                                class="nav-tabs px-0 text-decoration-none text-dark align-middle text-decoration-none text-dark">
+                                <i class="fs-4 bi-speedometer2"></i> <span
+                                    class="ms-1 d-none d-sm-inline">MBO/HBO</span> </a>
+                            <ul class="collapse show nav flex-column ms-1" id="opleiding" data-bs-parent="#menu">
+                                <li class="w-100">
+                                    <a href="#"
+                                        class="nav-tabs px-0 text-decoration-none text-dark text-decoration-none text-dark">
+                                        <span class="d-none d-sm-inline">MBO Software Development</span></a>
+                                </li>
+                                <li>
+                                    <a href="#"
+                                        class="nav-tabs px-0 text-decoration-none text-dark text-decoration-none text-dark">
+                                        <span class="d-none d-sm-inline">HBO Software Development</span></a>
+                                </li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="#vergoeding" data-bs-toggle="collapse"
+                                class="nav-tabs px-0 text-decoration-none text-dark align-middle text-decoration-none text-dark">
+                                <i class="fs-4 bi-bootstrap"></i> <span
+                                    class="ms-1 d-none d-sm-inline">Locaties</span></a>
+                            <ul class="collapse nav flex-column ms-1" id="vergoeding" data-bs-parent="#menu">
+                                <li class="w-100">
+                                    <a href="#"
+                                        class="nav-tabs px-0 text-decoration-none text-dark text-decoration-none text-dark">
+                                        <span class="d-none d-sm-inline">Amsterdam</span></a>
+                                </li>
+                                <li>
+                                    <a href="#" class="nav-tabs px-0 text-decoration-none text-dark"> <span
+                                            class="d-none d-sm-inline">Purmerend</span></a>
+                                </li>
+                                <li>
+                                    <a href="#" class="nav-tabs px-0 text-decoration-none text-dark"> <span
+                                            class="d-none d-sm-inline">Groningen</span></a>
+                                </li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="#cursus" data-bs-toggle="collapse"
+                                class="nav-tabs px-0 text-decoration-none text-dark align-middle">
+                                <i class="fs-4 bi-grid"></i> <span class="ms-1 d-none d-sm-inline">STAP-budget</span>
+                            </a>
+                            <ul class="collapse nav flex-column ms-1" id="cursus" data-bs-parent="#menu">
+                                <li class="w-100">
+                                    <a href="#" class="nav-tabs px-0 text-decoration-none text-dark"> <span
+                                            class="d-none d-sm-inline">Front-end Developer</span></a>
+                                </li>
+                                <li>
+                                    <a href="#" class="nav-tabs px-0 text-decoration-none text-dark"> <span
+                                            class="d-none d-sm-inline">Back-end Developer</span></a>
+                                </li>
+                                <li>
+                                    <a href="#" class="nav-tabs px-0 text-decoration-none text-dark"> <span
+                                            class="d-none d-sm-inline">Data Analist</span></a>
+                                </li>
+                                <li>
+                                    <a href="#" class="nav-tabs px-0 text-decoration-none text-dark"> <span
+                                            class="d-none d-sm-inline">Full Stack Web Developer</span></a>
+                                </li>
+                                <li>
+                                    <a href="#" class="nav-tabs px-0 text-decoration-none text-dark"> <span
+                                            class="d-none d-sm-inline">Data Engineer</span></a>
+                                </li>
+                                <li>
+                                    <a href="#" class="nav-tabs px-0 text-decoration-none text-dark"> <span
+                                            class="d-none d-sm-inline">ChatGPT expert</span></a>
+                                </li>
+                            </ul>
+                        <li>
+                        <li>
+                            <a href="#opleiders" data-bs-toggle="collapse"
+                                class="nav-tabs px-0 text-decoration-none text-dark align-middle">
+                                <i class="fs-4 bi-grid"></i> <span class="ms-1 d-none d-sm-inline">Pro
+                                    opleidingen</span> </a>
+                            <ul class="collapse nav flex-column ms-1" id="opleiders" data-bs-parent="#menu">
+                                <li class="w-100">
+                                    <a href="#" class="nav-tabs px-0 text-decoration-none text-dark"> <span
+                                            class="d-none d-sm-inline">Front-end Developer</span></a>
+                                </li>
+                                <li>
+                                    <a href="#" class="nav-tabs px-0 text-decoration-none text-dark"> <span
+                                            class="d-none d-sm-inline">Back-end Developer</span></a>
+                                </li>
+                                <li>
+                                    <a href="#" class="nav-tabs px-0 text-decoration-none text-dark"> <span
+                                            class="d-none d-sm-inline">Data Analist</span></a>
+                                </li>
+                                <li>
+                                    <a href="#" class="nav-tabs px-0 text-decoration-none text-dark"> <span
+                                            class="d-none d-sm-inline">Full Stack Web Developer</span></a>
+                                </li>
+                                <li>
+                                    <a href="#" class="nav-tabs px-0 text-decoration-none text-dark"> <span
+                                            class="d-none d-sm-inline">Data Engineer</span></a>
+                                </li>
+                                <li>
+                                    <a href="#" class="nav-tabs px-0 text-decoration-none text-dark"> <span
+                                            class="d-none d-sm-inline">ChatGPT expert</span></a>
+                                </li>
+                            </ul>
+                        <li>
+                            <a href="#"
+                                class="nav-tabs px-0 text-decoration-none text-dark align-middle text-decoration-none text-dark">
+                                <i class="fs-4 bi-table"></i> <span class="ms-1 d-none d-sm-inline">Voor
+                                    opleiders</span></a>
+                        </li>
+                        </li>
+                        <li>
+                            <a href="#" class="nav-tabs px-0 text-decoration-none text-dark align-middle">
+                                <i class="fs-4 bi-people"></i> <span class="ms-1 d-none d-sm-inline">Manifest</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#" class="nav-tabs px-0 text-decoration-none text-dark align-middle">
+                                <i class="fs-4 bi-people"></i> <span class="ms-1 d-none d-sm-inline">FAQ</span>
+                            </a>
+                        </li>
+                    </ul>
+                    <hr>
+                </div>
+            </div>
+            <div class="bg-light">
+            <div class="container mt-5">
+    <div class="card border-dark mb-3"  style="max-width: 55rem;" >
+        <div class="card-body text-success text-center bg-light">
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $selectedOption = isset($_POST["poll_option"]) ? $_POST["poll_option"] : "";
+
+    if ($selectedOption == "Option 1") {
+        $email = isset($_POST["email"]) ? $_POST["email"] : "";
+
+        echo "Bedankt voor je antwoord!" . PHP_EOL;
+        echo "Meer informatie zal worden verzonden naar: " . $email;
+        echo "<img src='academt.jpeg' alt='Je Afbeelding' class='img-fluid' style='max-width: 500px; margin: 0 auto;'>";
+    } else {
+        echo "Bedankt voor je antwoord!";
+        echo "<img src='academt.jpeg' alt='Je Afbeelding' class='img-fluid' style='max-width: 500px; margin: 0 auto;'>";
+    }
+} else {
+    echo "Error: Ongeldige toegang.";
+}
+
+?>
+</div>
+</div>
+</main>
+</div>
+        </header>
+</body>
+
+</html>
